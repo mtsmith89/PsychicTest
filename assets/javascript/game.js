bf14fd840ea360f5e43b66ae9d2881159bd9bd8e
Travis Smith <Travis.Smith2@gmail.com>
2018-03-14 20:35:52 -0400

    2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -80,9 +80,9 @@ document.onkeyup = function(event) {
   }
 };
 
-document.getElementById('openKeyboard').addEventListener('click', function(){
-  var inputElement = document.getElementById('hiddenInput');
-  inputElement.style.visibility = 'visible'; // unhide the input
-  inputElement.focus(); // focus on it so keyboard pops
-  inputElement.style.visibility = 'hidden'; // hide it again
-});
+window.onload=function(){
+
+prompt();
+
+};
+
